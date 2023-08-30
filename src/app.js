7356aa0ee3da0c2b0bd9d108998fc31683b69b8b
souzaarthur <arthursouzaoliveira42@gmail.com>
2023-08-29 23:58:26 -0300

    feature: adding post method

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const bookListMocked = [
     {id: 1, "bookName": "Bible"},
     {id: 2, "bookName": "Hitchicker's guide to the galaxy"},
 ];
 
-app.get("/", (request, response) => {
-    response.status(200).send("Node Course");
+app.get("/", (req, res) => {
+    res.status(200).send("Node Course");
 });
 
-app.get("/books", (request, response) => {
-    response.status(200).json(bookListMocked);
+app.get("/books", (req, res) => {
+    res.status(200).json(bookListMocked);
+});
+
+app.post("/books", (req, res) => {
+    bookListMocked.push(req.body);
+    res.status(201).send("Livro adicionado com sucesso");
 });
 
 export default app;
